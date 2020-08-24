e7a651312a1277a7eb88320c892ba50c6871cd49
Onore849 <rion3376@gmail.com>
2020-08-24 20:57:22 +0900

    Initial Commit

---
+++ GridView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridView
+//
+//  Created by 野澤拓己 on 2020/08/24.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
